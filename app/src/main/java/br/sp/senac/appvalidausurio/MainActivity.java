2bcbad3232935692b447e32751b190ea935a1f9d
samuel.pssantos <saamuka70@gmail.com>
2019-08-06 16:59:11 -0300

    Initial commit

---
+++ app/src/main/java/br/sp/senac/appvalidausurio/MainActivity.java
@@ -0,0 +1,62 @@
+package br.sp.senac.appvalidausurio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText txtUsuario, txtSenha;
+    Button btnEntrar, btnSair;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        txtUsuario = (EditText) findViewById(R.id.txtUsuario);
+        txtSenha = (EditText) findViewById(R.id.txtSenha);
+
+        btnEntrar = (Button) findViewById(R.id.btnEntrar);
+        btnSair = (Button) findViewById(R.id.btnSair);
+
+        txtUsuario.requestFocus();
+
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String usuario, senha;
+                usuario = txtUsuario.getText().toString();
+                senha = txtSenha.getText().toString();
+
+                if (usuario.equals("senac") && senha.equals("senac")) {
+
+                    startActivity(new Intent(getApplicationContext(), RespondeUsuario_Activity.class));
+                    finish();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Usuário ou senha invalidos", Toast.LENGTH_SHORT).show();
+                    txtUsuario.setText("");
+                    txtSenha.setText("");
+                    txtUsuario.requestFocus();
+
+                }
+
+            }
+        });
+
+        btnSair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+    }
+}
