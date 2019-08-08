1905e352be5a756737c6a19a8b320e1ef4cde0d0
samuel.pssantos <saamuka70@gmail.com>
2019-08-08 17:20:21 -0300

    Atualizado 17:20

---
+++ app/src/main/java/br/sp/senac/appvalidausurio/Splash_Activity.java
@@ -0,0 +1,25 @@
+package br.sp.senac.appvalidausurio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_activity);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        }, 2000);
+    }
+}
